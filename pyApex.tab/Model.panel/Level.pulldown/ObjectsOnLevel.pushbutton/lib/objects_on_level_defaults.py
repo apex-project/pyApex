cf26560c3362de400c80f9538ee91223ec391131
Alex Melnikov <alex@hiteca.ru>
2017-12-29 14:09:48 +0300

    Remove lib from gitignore

---
+++ pyApex.tab/Model.panel/Level.pulldown/ObjectsOnLevel.pushbutton/lib/objects_on_level_defaults.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+exceptions = [
+    "Levels",
+    "Work Plane Grid",
+    "Viewports",
+    "Other",
+]
+
+limit = 50
+
+def _filter_list(l):
+    l = filter(lambda x: len(x) > 0, l)
+    l = map(lambda x: x.strip("\n").strip("\n").strip("\r"), l)
+    return l
+
+
+def exceptions2str(l):
+    """"""
+
+    """back compatibility START"""
+    if type(l) != list:
+        l = exceptions2list(l)
+    """back compatibility END"""
+
+    l = _filter_list(l)
+    return "\n".join(l)
+
+
+def exceptions2list(s):
+    l = s.split("\n")
+
+    """back compatibility START"""
+    # remove empty rows
+    l = _filter_list(l)
+    # check is separator is ',' - not '\n'
+    if len(l) == 1:
+        if l[0].indexOf(",") != -1:
+            l = l[0].split(",")
+            l = filter(lambda x: len(x) > 0, l)
+    """back compatibility END"""
+
+    l = _filter_list(l)
+
+    return l
