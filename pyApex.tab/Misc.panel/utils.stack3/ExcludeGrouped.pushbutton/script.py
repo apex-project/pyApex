707b143e0c6cac523de062839724d52ec2a3ae2b
melnikovalex <alex@hiteca.ru>
2019-05-07 18:44:59 +0200

    Feature: exclude grouped

---
+++ pyApex.tab/Misc.panel/utils.stack3/ExcludeGrouped.pushbutton/script.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__title__ = 'Exclude Grouped'
+__doc__ = """Exclude grouped elements from selection
+
+Shift+Click - isolate grouped elements"""
+
+__helpurl__ = "https://apex-project.github.io/pyApex/help#exclude-grouped"
+
+from pyrevit import revit
+selection = revit.get_selection()
+
+def filter_grouped(e, inverse = False):
+    if inverse:
+        return e.GroupId.IntegerValue != -1
+    else:
+        return e.GroupId.IntegerValue == -1
+
+if __name__ == '__main__':
+    sel = selection.elements
+    sel = list(filter(lambda e: filter_grouped(e, __shiftclick__), sel))
+    selection.set_to(sel)
